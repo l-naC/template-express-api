1f12c903b90cfc40d57c5b4a21b7d2c5fc9a44a3
Léna <lena.clavier@gmail.com>
2020-07-03 14:13:28 +0200

    [FEAT] Add connection DB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { testEnvironmentVariable } from '../settings';
+import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
-
-indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
+indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
